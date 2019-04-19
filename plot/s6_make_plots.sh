08652c989d105a24bbbffc27271e6308a52a2c20
gajjarv <vishalg@berkeley.edu>
2019-04-19 11:41:41 +0800

    plot for specified file

--- plot/s6_make_plots.sh
+++ plot/s6_make_plots.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
+fitsfile=$1
+echo ${fitsfile}
+if [ ! -e $1.plotted ]
+then
+	echo ...waterfall
+	python /usr/local/bin/s6_plot_waterfall.py ${fitsfile}
+	echo ...power histogram
+	python /usr/local/bin/s6_plot_power_histogram.py ${fitsfile}
+	echo ...baseband
+	python /usr/local/bin/s6_plot_baseband.py ${fitsfile}
+	echo ...meanpower
+	python /usr/local/bin/s6_plot_meanpower.py ${fitsfile}
+	touch $1.plotted
+fi
 
-for fitsfile in `ls *.fits`; do
-    echo $fitsfile
-	if [ ! -e $fitsfile.plotted ]
-	then
-		echo ...waterfall
-		python /usr/local/bin/s6_plot_waterfall.py $fitsfile
-		echo ...power histogram
-		python /usr/local/bin/s6_plot_power_histogram.py $fitsfile
-		echo ...baseband
-		python /usr/local/bin/s6_plot_baseband.py $fitsfile
-		touch $fitsfile.plotted
-	fi
-done
+
